a036653ab51016d6f2e92511c00f43fb24f028e5
GenesizVitual <lastfandiansyah@gmail.com>
2022-02-18 10:17:46 +0800

    Penambahan screen jika tidak ditemukan solusi

--- lib/screen/NotFoundSolution.dart
+++ lib/screen/NotFoundSolution.dart
@@ -6,7 +6,23 @@ class NotFountSolution extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Not Found Solution"),
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 300,
+            child: FittedBox(
+              child: Image.asset("assets/img/doctor_confius.jpg"),
+            ),
+          ),
+          Text(
+            'Skin care tidak ditemukan yang sesaui dengan ciri-ciri kulit anda.',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
     );
   }
 }
